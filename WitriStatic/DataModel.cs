ef4f73e0b111bfd269d2d50d93eab4a11dd08806
Silviu.Ganea <silviu.ganea@continental-corporation.com>
2015-10-02 14:10:33 +0300

    Added ModelParser and DataModel classes

---
+++ WitriStatic/DataModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WitriStatic
+{
+    public class DataModel
+    {
+        public static Dictionary<string, Window> windowDict;
+        public static Dictionary<string, Widget> widgetDict;
+        public static Dictionary<string, Compositor> compositorDict;
+        public static Dictionary<string, Layer> layerDict;
+        
+        public class Widget
+        {
+            public string Name { get; set; }
+            public string ID { get; set; }
+            public XElement xelem { get; set; }
+
+            public Widget(string name)
+            {
+                this.Name = name;
+            }
+            public Widget(string name, XElement xelem)
+            {
+                this.Name = name;
+                this.xelem = xelem;
+            }         
+        }
+        public class Buflet : Widget
+        {
+            public Buflet(string name) : base(name)
+            {
+            }
+            public Buflet(string name, XElement xelem) : base(name)
+            {
+            }
+            Dictionary<string, Section> sections;
+        }
+        public class Section : Widget
+        {
+            public Section(string name) : base(name)
+            {
+            }
+            public Section(string name, XElement xelem) : base(name)
+            {
+            }
+        }
+        public class Window : Widget
+        {
+            public Window(string name) : base(name)
+            {
+            }
+            public Window(string name, XElement xelem) : base(name)
+            {
+            }
+            Dictionary<string, Buflet> buflets;
+        }
+        public class Compositor : Widget
+        {
+            public Compositor(string name) : base(name)
+            {
+            }
+            public Compositor(string name, XElement xelem) : base(name)
+            {
+            }
+            Dictionary<string, Layer> layers;
+        }
+        public class Layer : Widget
+        {
+            public Layer(string name) : base(name)
+            {
+            }
+            public Layer(string name, XElement xelem) : base(name)
+            {
+            }
+            Dictionary<string, Section> sections;
+        }   
+    }
+}
--- WitriStatic/Program.cs
+++ WitriStatic/Program.cs
@@ -10,75 +10,15 @@ namespace WitriStatic
 {
     class Program
     {
-        public static Dictionary<string, Data.Window> windowDictionary;
-        public static Dictionary<string, Compositor> compositorDictionary;
-        public static Dictionary<string, Layer> layerDictionary;
-
         static void Main(string[] args)
         {
-           loadDictionaries();
-        }
-        public static void loadDictionaries()
-        {
-            string ciaFolderPath = getCiaFolderPath_fromProjectConfig();
-            windowDictionary = getWindowDictionary(ciaFolderPath);
+
+            DataModel dataModel = new DataModel();
+            ModelParser.loadXmlDataIntoModel(dataModel);
+            HppParser.loadId(dataModel);
         }
 
-        private static Dictionary<string, Window> getWindowDictionary(string ciaFolderPath)
-        {
-            //search for <window> elements in all the .xml files in 10_CIA folder
-            foreach (var xmlFile in Directory.EnumerateFiles(ciaFolderPath))
-            {
-                XDocument xdoc = XDocument.Load(xmlFile);
-                foreach (XElement window in xdoc.Descendants("Window"))
-                {
-                    string windowName = string.Empty;
-                    if(window.Attribute("Name") != null){
-                        windowName = window.Attribute("Name").Value;
-                        //if the window dictionary doesn't contain the element, create a window object and add it to the dictionary
-                        if (!windowDictionary.Keys.Contains(windowName))
-                        {
-                            //window.Descendants("Buflet").Select(e => e.Attribute("Name").Value).ToList();
-                            Window tempWindow = new Window(windowName);
-                        }
-                    }
-                }
-            }
-            return new Dictionary<string, Window>();
-        }
-        public static string getCiaFolderPath_fromProjectConfig()
-        {
-            string relativePath = @"..\..\..\pkg\xcmodel\mdl\ProjectConfig.xml";
-            string fullPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), relativePath));
-            
-            XElement pjConfig_xdoc = XElement.Load(relativePath);
-
-
-            string tempCiaPath = @"..\..\brutus\adapt\model\10_CIA\";
-            string tempfullPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), tempCiaPath));
-            return tempfullPath;
-        }
-        public static bool isDetached(string widgetName)
-        {
-            bool result = false;
-
-
-            return result;
-        }
-        public static string getDetachedWidgetLayer(string widgetName)
-        {
-            string result = string.Empty;
-
-
-
-            return result;
-        }
-        public static string getDetachedWidgetCompositor(string widgetName)
-        {
-            string result = string.Empty;
-
-            return result;
-        }
+        
     }
     
 }
